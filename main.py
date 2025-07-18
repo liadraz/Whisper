5a86498702eae86a09cffe1a5ccb6cd880e8834b
liad.raz <liadraz@users.noreply.github.com>
2025-07-18 13:50:34 +0300

    run main safely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 from routers.webhook_router import webhook_router
 
 app = FastAPI()
 
 app.include_router(webhook_router.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
